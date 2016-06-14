1ec3b15d475cce4cc772383c80e5bd6676843fc1
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-06-14 12:02:41 -0400

    initial commit

---
+++ client/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello there!</div>
+  );
+};
+
+// After Meteor loads in the browser, render my app to the DOM.
+Meteor.startup(() => {
+  ReactDOM.render(<App/>, document.querySelector('.container'));
+});
