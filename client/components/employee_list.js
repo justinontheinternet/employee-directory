124145c82aac10306071da9f4a8d0679421f30bb
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-06-15 10:35:39 -0400

    refined onClick so 'Load More' works as it should

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -4,6 +4,7 @@ import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
 const PER_PAGE = 20;
+let total_cards = PER_PAGE;
 // props.employees => an array of employee objects
 const EmployeeList = (props) => {
   return(
@@ -13,7 +14,7 @@ const EmployeeList = (props) => {
           <EmployeeDetail employee={employee} key={employee._id} />
         )}
       </div>
-      <button className="btn btn-primary" onClick={() => Meteor.subscribe('employees', 40)}>
+      <button className="btn btn-primary" onClick={() => Meteor.subscribe('employees', total_cards += PER_PAGE)}>
         Load More...
       </button>
     </div>
@@ -27,5 +28,5 @@ export default createContainer(() => {
   // return an object. Whatever we return will be sent to EmployeeList as props
     // .fetch() is what actually executes the query
     // This is only executing the search on the client side.
-  return { employees: Employees.find({}). fetch() };
+  return { employees: Employees.find({}).fetch() };
 }, EmployeeList);
