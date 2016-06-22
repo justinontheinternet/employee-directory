6caa33b0d291597a156235e31f6cd810a317ecca
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-06-22 17:47:19 -0400

    converted employeelist to class-based component

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
 const PER_PAGE = 20;
 let total_cards = PER_PAGE;
-// props.employees => an array of employee objects
-const EmployeeList = (props) => {
-  return(
-    <div>
-      <div className="employee-list">
-        {props.employees.map(employee =>
-          <EmployeeDetail employee={employee} key={employee._id} />
-        )}
+
+// this.props.employees => an array of employee objects
+class EmployeeList extends Component {
+  componentWillMount() {
+    this.page = 1;
+  }
+
+  handleButtonClick() {
+    Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+    this.page += 1;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="employee-list">
+          {this.props.employees.map(employee =>
+            <EmployeeDetail employee={employee} key={employee._id} />
+          )}
+        </div>
+        <button className="btn btn-primary" onClick={this.handleButtonClick.bind(this)}>
+          Load More...
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={() => Meteor.subscribe('employees', total_cards += PER_PAGE)}>
-        Load More...
-      </button>
-    </div>
-  )
+    );
+  }
 };
 // In a React app, we get access to subscriptions by setting up a container
   // A container watches a subscription, and whenever it changes, it passes the info to a component, causing it to re-render
